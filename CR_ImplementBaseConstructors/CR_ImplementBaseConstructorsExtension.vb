44d2ca70c2786a1f456b109a99be16b5fb67d3d3
Rory Becker <RoryBecker@gmail.com>
2014-12-08 12:58:50 +0000

    Initial Load

---
+++ CR_ImplementBaseConstructors/CR_ImplementBaseConstructorsExtension.vb
@@ -0,0 +1,7 @@
+Imports System.ComponentModel.Composition
+Imports DevExpress.CodeRush.Common
+
+<Export(GetType(IVsixPluginExtension))> _
+  Public Class CR_ImplementBaseConstructorsExtension
+    Implements IVsixPluginExtension
+End Class
